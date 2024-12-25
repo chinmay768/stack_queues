bcc5c6c94d0f6c30c45c4917b04fb9c5ec11b501
chinmay768 <chinmaysahani07@gmail.com>
2024-12-25 23:42:21 +0530

    Validate Stack Sequences

--- misc/src/MaximumNestingDepthParentheses.java
+++ misc/src/MaximumNestingDepthParentheses.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class MaximumNestingDepthParentheses {
+
+    public static int maximumNestingDepthNaive(String str){
+        int max = 0;
+
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '('){
+                st.push(str.charAt(i));
+                max = Math.max(max, st.size());
+            }else if(str.charAt(i) == ')') {
+                st.pop();
+            }
+        }
+
+        return max;
+    }
+
+    public static int maximumNestingDepth(String str){
+        int max = 0;
+        int curr = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '('){
+                curr++;
+                max = Math.max(curr, max);
+            }else if(str.charAt(i) == ')') {
+                curr--;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String s =
+                "(1+(2*3)+((8)/4))+1";
+
+        System.out.println(maximumNestingDepth(s));
+    }
 }
--- misc/src/ValidateStackSequences.java
+++ misc/src/ValidateStackSequences.java
@@ -1,2 +1,43 @@
+import java.util.Stack;
+
 public class ValidateStackSequences {
+
+    public static boolean validateStackSequencesNaive(int[] pushed, int[] popped){
+        Stack<Integer> st = new Stack<>();
+
+        int popIdx = 0;
+        for(int i = 0; i < pushed.length; i++){
+            st.push(pushed[i]);
+
+            while (!st.isEmpty() &&st.peek() == popped[popIdx]){
+                st.pop();
+                popIdx++;
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static boolean validateStackSequences(int[] pushed, int[] popped){
+        int top = -1;
+        int j = 0;
+        for(int i = 0; i < pushed.length; i++){
+            top++;
+            pushed[top] = pushed[i];
+
+            while (top >= 0 && pushed[top] == popped[j]){
+                top--;
+                j++;
+            }
+        }
+
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+        int[] pushed = {1,2,3,4,5};
+        int[] popped = {4,3,5,1,2};
+
+        System.out.println(validateStackSequences(pushed, popped));
+    }
 }
