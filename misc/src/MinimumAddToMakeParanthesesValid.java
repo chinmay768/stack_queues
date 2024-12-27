40edf84ea8e8e79dc7ed69bbddeaa21036f69477
chinmay768 <chinmaysahani07@gmail.com>
2024-12-27 23:31:45 +0530

    Min Add To Make Parantheses Valid

--- misc/src/MinimumAddToMakeParanthesesValid.java
+++ misc/src/MinimumAddToMakeParanthesesValid.java
@@ -2,7 +2,23 @@ import java.util.Stack;
 
 public class MinimumAddToMakeParanthesesValid {
 
+    public static int minimumAddToMakeParanthesesValidNaive(String str){
+        Stack<Character> st = new Stack<>();
 
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '('){
+                st.push(str.charAt(i));
+            }else{
+                if(!st.isEmpty() && st.peek() == '('){
+                    st.pop();
+                }else {
+                    st.push(str.charAt(i));
+                }
+            }
+        }
+
+        return st.size();
+    }
 
     public static int minimumAddToMakeParanthesesValid(String str){
         int open = 0;
